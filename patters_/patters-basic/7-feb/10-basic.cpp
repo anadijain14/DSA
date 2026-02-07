4b078dd75824ae9ed494466c303c160753e6bece
anadijain14 <anadisethi693@gmail.com>
2026-02-07 21:39:54 +0530

    notes

--- patters_/patters-basic/7-feb/10-basic.cpp
+++ patters_/patters-basic/7-feb/10-basic.cpp
@@ -9,7 +9,7 @@ int main() {
         for(int j=1;j<=n-i;j++){
             cout<<"  ";
         }
-        //left pattern
+        //left patterna
         for(int j=i;j<=2*i-1;j++){
             cout<<j<<" ";
         }
