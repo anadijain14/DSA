6259d9e6a73ef800e25670d05a70a91ace200825
anadijain14 <anadisethi693@gmail.com>
2026-02-12 13:19:53 +0530

    notes

---
+++ classs.1/1_stquetion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;        
+int main (){
+    char n;
+    cinn>>n;
+    if (a>=z);{
+    cout>> lowercase;
+}
+else if (a>=A){
+    cout>>uppercase;
+}
+
+
+return 0;
+}
