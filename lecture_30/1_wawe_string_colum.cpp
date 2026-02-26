0dff394d693f7610069df2b832ba1c2461197328
anadijain14 <anadisethi693@gmail.com>
2026-02-26 14:17:21 +0530

    notes

--- lecture_30/1_wawe_string_colum.cpp
+++ lecture_30/1_wawe_string_colum.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int r,c;
     cin>>r>>c;
-    vector<vectoe<int>arr(r,vector<int>(c));
+    vector<vector<int>>arr(r,vector<int>(c));
     for(int i=0;i<r;i++){
         for (int j=0;j<c;j++){
             cin>>arr[i][j];
