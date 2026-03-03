a882b503ce38e4e2d48dbd262d6fd38a9ed9729e
anadijain14 <anadisethi693@gmail.com>
2026-03-03 19:32:02 +0530

    notes

--- assignment_4/4_square_ofsorted_array.cpp
+++ assignment_4/4_square_ofsorted_array.cpp
@@ -53,44 +53,6 @@
 
 
 
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-
-// vector<int> sortedSquares(vector<int>& nums) {
-//     int n = nums.size();
-//     vector<int> ans(n);
-
-//     int i = 0;          // left pointer
-//     int j = n - 1;      // right pointer
-//     int k = n - 1;      // position to fill from back
-
-//     while (i <= j) {
-//         int a = nums[i] * nums[i];
-//         int b = nums[j] * nums[j];
-
-//         if (a > b) {
-//             ans[k] = a;
-//             i++;
-//         } else {
-//             ans[k] = b;
-//             j--;
-//         }
-//         k--;
-//     }
-
-//     return ans;
-// }
-
-// int main() {
-//     vector<int> nums = {-4, -1, 0, 3, 10};
-//     vector<int> result = sortedSquares(nums);
-
-//     for (int x : result) {
-//         cout << x << " ";
-//     }
-// }
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -99,23 +61,64 @@ vector<int> sortedSquares(vector<int>& nums) {
     int n = nums.size();
     vector<int> ans(n);
 
-    int left = 0;          // left pointer
-    int right = n - 1;      // right pointer
+    int i = 0;          // left pointer
+    int j = n - 1;      // right pointer
     int k = n - 1;      // position to fill from back
 
-    while (left <= right) {
-        int a = nums[left] * nums[left];
-        int b = nums[right] * nums[right];
+    while (i <= j) {
+        int a = nums[i] * nums[i];
+        int b = nums[j] * nums[j];
 
         if (a > b) {
             ans[k] = a;
-            left++;
+            i++;
         } else {
             ans[k] = b;
-            right--;
+            j--;
         }
         k--;
     }
 
     return ans;
 }
+
+int main() {
+    vector<int> nums = {-4, -1, 0, 3, 10};
+    vector<int> result = sortedSquares(nums);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+}
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main(){
+
+// vector<int> sortedSquares(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> ans(n);
+
+//     int left = 0;          // left pointer
+//     int right = n - 1;      // right pointer
+//     int k = n - 1;      // position to fill from back
+
+//     while (left <= right) {
+//         int a = nums[left] * nums[left];
+//         int b = nums[right] * nums[right];
+
+//         if (a > b) {
+//             ans[k] = a;
+//             left++;
+//         } else {
+//             ans[k] = b;
+//             right--;
+//         }
+//         k--;
+//     }
+
+//     return ans;
+// }
+// }
