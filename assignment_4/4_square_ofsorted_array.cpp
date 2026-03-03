e59b5863f848043d4954899caa5b5fe8d324d084
anadijain14 <anadisethi693@gmail.com>
2026-03-03 19:03:32 +0530

    notes

--- assignment_4/4_square_ofsorted_array.cpp
+++ assignment_4/4_square_ofsorted_array.cpp
@@ -1,6 +1,21 @@
-#include <iosteream>
-using namespace std;
-
-int main(){
-    
-}
+#include <iostream>
+#include <vector>
+using namespace std;  
+int main() {
+    int left = 0, right, n-1;  
+    int i=n-1;
+    cin >> n;
+    vector<int> nums(n);
+          for (int i = 0, j = n - 1, k = n - 1; i <= j; --k) {
+            int a = nums[i] * nums[i];
+            int b = nums[j] * nums[j];
+            if (a > b) {
+                ans[k] = a;
+                ++i;
+            } else {
+                ans[k] = b;
+                --j;
+            }
+        }
+        return ans;
+    }
