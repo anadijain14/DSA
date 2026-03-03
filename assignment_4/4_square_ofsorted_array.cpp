cbd536367c1d4707c1ed48821a3998a43713930c
anadijain14 <anadisethi693@gmail.com>
2026-03-03 19:18:23 +0530

    notes

--- assignment_4/4_square_ofsorted_array.cpp
+++ assignment_4/4_square_ofsorted_array.cpp
@@ -22,31 +22,100 @@
 
 
 
+// #include <iostream>
+// #include <vector>           
+// using namespace std;
+
+// vector<int> sortedSquares(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> ans(n);
+
+//     int left = 0, right = n - 1;
+
+//      (left <= right) {
+//         int a = nums[left] * nums[left];
+//         int b = nums[right] * nums[right];
+
+    
+//     for (int i = 0, j = n - 1, k = n - 1; i <= j; --k) {
+//         int a = nums[i] * nums[i];
+//         int b = nums[j] * nums[j];
+//         if (a > b) {
+//             ans[k] = a;
+//             ++i;
+//         } else {
+//             ans[k] = b;
+//             --j;
+//         }
+//     }
+//     return ans;
+// }
+
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// vector<int> sortedSquares(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> ans(n);
+
+//     int i = 0;          // left pointer
+//     int j = n - 1;      // right pointer
+//     int k = n - 1;      // position to fill from back
+
+//     while (i <= j) {
+//         int a = nums[i] * nums[i];
+//         int b = nums[j] * nums[j];
+
+//         if (a > b) {
+//             ans[k] = a;
+//             i++;
+//         } else {
+//             ans[k] = b;
+//             j--;
+//         }
+//         k--;
+//     }
+
+//     return ans;
+// }
+
+// int main() {
+//     vector<int> nums = {-4, -1, 0, 3, 10};
+//     vector<int> result = sortedSquares(nums);
+
+//     for (int x : result) {
+//         cout << x << " ";
+//     }
+// }
+
 #include <iostream>
-#include <vector>           
+#include <vector>
 using namespace std;
 
 vector<int> sortedSquares(vector<int>& nums) {
     int n = nums.size();
     vector<int> ans(n);
 
-    int left = 0, right = n - 1;
+    int left = 0;          // left pointer
+    int right = n - 1;      // right pointer
+    int k = n - 1;      // position to fill from back
 
     while (left <= right) {
         int a = nums[left] * nums[left];
         int b = nums[right] * nums[right];
 
-    
-    for (int i = 0, j = n - 1, k = n - 1; i <= j; --k) {
-        int a = nums[i] * nums[i];
-        int b = nums[j] * nums[j];
         if (a > b) {
             ans[k] = a;
-            ++i;
+            left++;
         } else {
             ans[k] = b;
-            --j;
+            right--;
         }
+        k--;
     }
+
     return ans;
 }
