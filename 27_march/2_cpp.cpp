fb7294d225845aa31ff96b0e6b0911ccd8bd8230
anadijain14 <anadisethi693@gmail.com>
2026-03-27 22:47:21 +0530

    notes

---
+++ 27_march/2_cpp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> &arr, int target) {
+    int left = 0, right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {2, 4, 6, 8, 10};
+    int target = 8;
+
+    int result = binarySearch(arr, target);
+
+    if (result != -1)
+        cout << "Element found at index " << result;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
