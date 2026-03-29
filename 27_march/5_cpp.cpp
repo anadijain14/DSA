9419fd601183cf86a8f977dbb0071f3730be36ae
anadijain14 <anadisethi693@gmail.com>
2026-03-29 21:22:02 +0530

    notes

---
+++ 27_march/5_cpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int marks;
+
+public:
+    Student(string n, int m) {
+        name = n;
+        marks = m;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student s1("Anadi", 95);
+    s1.display();
+
+    return 0;
+}
